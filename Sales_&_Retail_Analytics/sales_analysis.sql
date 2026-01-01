d9480734723982f6c924626404a27801bd9aeee3
Zohaib Sattar <zabizubi86@gmail.com>
2026-01-01 08:35:34 +0500

    Add SQL query to select customer data

--- Sales_&_Retail_Analytics/sales_analysis.sql
+++ Sales_&_Retail_Analytics/sales_analysis.sql
@@ -4,3 +4,7 @@
 -- Dataset: sales_dataset_30days.csv
 -- Purpose: 1-Month Daily SQL Practice
 -- ==================================================
+
+-- Select Table From Database
+select * from Analysis_sql
+where customer_id=164;
