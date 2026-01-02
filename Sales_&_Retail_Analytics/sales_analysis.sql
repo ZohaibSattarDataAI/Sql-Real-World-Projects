c5caa074a325082d7be8fcad303aea7ec278c314
Zohaib Sattar <zabizubi86@gmail.com>
2026-01-02 15:18:37 +0500

    Update sales_analysis.sql

--- Sales_&_Retail_Analytics/sales_analysis.sql
+++ Sales_&_Retail_Analytics/sales_analysis.sql
@@ -8,3 +8,9 @@
 -- Select Table From Database
 SELECT * FROM Analysis_sql
 WHERE customer_id=164;
+
+-- Day 2: Total revenue per product
+SELECT product_name, SUM(total_amount) AS Total_Revenue 
+FROM Analysis_sql
+GROUP BY product_name
+ORDER BY Total_Revenue DESC;
