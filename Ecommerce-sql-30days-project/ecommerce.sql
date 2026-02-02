5a3c8a9eb9fe41804b157b7c08d3120ad91e194b
Zohaib Sattar <zabizubi86@gmail.com>
2026-02-02 15:00:40 +0500

    Update ecommerce.sql

--- Ecommerce-sql-30days-project/ecommerce.sql
+++ Ecommerce-sql-30days-project/ecommerce.sql
@@ -1,7 +1,9 @@
-##1:Total users signed up in last 30 days
+#1:Total users signed up in last 30 days
 
 SELECT COUNT(*) AS new_users_30d
 FROM users
 WHERE signup_date >= date('now','-30 days');
 
-### 
+### Explanation:
+### Counts all users who signed up in the last 30 days.
+### Helps track new user acquisition and growth trends.
