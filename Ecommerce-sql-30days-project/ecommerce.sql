9f2916bedfcfe95296231c93eef71980e7c62815
Zohaib Sattar <zabizubi86@gmail.com>
2026-02-03 19:41:37 +0500

    Update ecommerce.sql

--- Ecommerce-sql-30days-project/ecommerce.sql
+++ Ecommerce-sql-30days-project/ecommerce.sql
@@ -8,7 +8,7 @@ WHERE signup_date >= date('now','-30 days');
 ### Counts all users who signed up in the last 30 days.
 ### Helps track new user acquisition and growth trends.
 
-# Users with no orders in last 30 days
+# 2:Users with no orders in last 30 days
 SELECT *
 FROM users
 WHERE user_id NOT IN (SELECT DISTINCT user_id FROM orders);
@@ -16,3 +16,12 @@ WHERE user_id NOT IN (SELECT DISTINCT user_id FROM orders);
 ### Explanation:
 ### Lists users who never placed an order.
 ### Identifies inactive users for retention campaigns.
+
+# 3. Users with only 1 order
+  
+SELECT user_id, COUNT(order_id) AS order_count
+FROM orders
+GROUP BY user_id
+HAVING COUNT(order_id) = 1;
+
+
